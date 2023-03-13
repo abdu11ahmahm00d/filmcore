a81b04486d364fcde6c77c998d6f7ae4ee8223f4
Abdullah Mahmood <122725447+abdu11ahmahm00d@users.noreply.github.com>
2023-03-13 09:53:07 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Filmcore by Abdullah" key="title"/>
+        <meta property="og:description" content="Built by Abdullah" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://filmcore.vercel.app/"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
